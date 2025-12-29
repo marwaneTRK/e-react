2ef77930b36b06a1dc0a7ceaec7ed54a10f1744c
marwaneTRK <marwantraiki@gmail.com>
2025-12-29 23:01:48 +0100

    footer done

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 mt-10 p-6">
+      <div className="max-w-2xl mx-auto">
+        <h3 className="mb-2 text-center">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
+          laudantium autem quod enim, soluta in, neque ad nam quo optio non
+          eaque sed. Minus nisi accusantium dicta voluptatem et fuga.
+        </h3>
+      </div>
+
+      <p className="text-center text-sm text-gray-500 mt-4">© 2025 MyShop</p>
+    </footer>
+  );
+}
